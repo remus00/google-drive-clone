600de6db5ae6037d08b5f104677aed76f04076b7
remus-wolico <135813551+remus00@users.noreply.github.com>
2024-11-08 16:19:51 +0100

    update global css

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
     return (
-        <div className="flex h-screen w-full items-center justify-center bg-amber-100">
+        <div className="flex-center h-screen">
             <h1 className="text-3xl text-brand-100">StoreIt - the only storage you need</h1>
         </div>
     );
