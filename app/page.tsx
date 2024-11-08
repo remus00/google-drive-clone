fc8c248b022399d8df5ebb825bf42278e1a6a2e0
remus-wolico <135813551+remus00@users.noreply.github.com>
2024-11-08 16:16:25 +0100

    add prettier

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-    return <div className="text-blue">Store</div>;
+    return (
+        <div className="flex h-screen w-full items-center justify-center bg-amber-100">
+            <h1 className="text-3xl text-brand-100">StoreIt - the only storage you need</h1>
+        </div>
+    );
 }
