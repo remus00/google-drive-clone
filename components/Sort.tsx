fefdaefdbf180d33cac64e93c4a76cc27b294e6a
remus-wolico <135813551+remus00@users.noreply.github.com>
2024-11-09 14:13:14 +0100

    sort functionality

--- components/Sort.tsx
+++ components/Sort.tsx
@@ -1,5 +1,41 @@
+'use client';
+
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from '@/components/ui/select';
+import { sortTypes } from '@/constants';
+import { usePathname, useRouter } from 'next/navigation';
+
 const Sort = () => {
-    return <div>Sort</div>;
+    const router = useRouter();
+    const path = usePathname();
+
+    const handleSort = (value: string) => {
+        router.push(`${path}?sort=${value}`);
+    };
+
+    return (
+        <Select onValueChange={handleSort} defaultValue={sortTypes[0].value}>
+            <SelectTrigger className="sort-select">
+                <SelectValue placeholder={sortTypes[0].label} />
+            </SelectTrigger>
+            <SelectContent className="sort-select-content">
+                {sortTypes.map((sort) => (
+                    <SelectItem
+                        key={sort.label}
+                        value={sort.value}
+                        className="shadn-select-item"
+                    >
+                        {sort.label}
+                    </SelectItem>
+                ))}
+            </SelectContent>
+        </Select>
+    );
 };
 
 export default Sort;
